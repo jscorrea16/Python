c935dfc3ae7ee543120c51d5761bd36fdeb164cd
Joshua Correa <jscorrea1@buffs.wtamu.edu>
2023-06-21 22:19:16 -0500

    Update

--- app_lists.py
+++ app_lists.py
@@ -2,9 +2,14 @@
 # A collection allows us to put many variables in a "single variable."
 # A collection is nice because we can carry all many values around in one convenient package.
 
+# Lists and definite loops - best pals
 friends = ['Joseph', 'Glenn', 'Sally']
 for friend in friends:
     print('Happy New Year:', friend)
 print('Done!')
 
-carryon = ['socks', 'shirt', 'perfume']
+z = ['Joseph', 'Glenn', 'Sally']
+for x in z:
+    print('Happy New Year:', x)
+print('Done!')
+# Stopped a 5:14 on 6/21/2023
